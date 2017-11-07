3d12ff21a5366af725c67220c301c3c9be66669d
liushuo <liushuo@ect888.com>
2017-11-07 19:49:08 +0800

    正则表达式获得图片并下载

--- ScrapyDemo/debug.py
+++ ScrapyDemo/debug.py
@@ -1,5 +1,6 @@
 __author__ = 'liushuo'
 from scrapy.cmdline import execute
-
+#from scrapy import cmdline
 #命令行运行scrapy库相当于运行此命令;run configuration中配置script params为crawl spidername
+cmd = 'scrapy crawl ZhihuSpider'
 execute()
